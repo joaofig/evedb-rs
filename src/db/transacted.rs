0a83f8bc185b32c0d15294643dc0a605db5eec64
joaofig <joao.figueira@gmail.com>
2025-12-18 09:26:05 +0000

    Refactor `Transacted` struct and simplify `execute` method

--- src/db/transacted.rs
+++ src/db/transacted.rs
@@ -1,6 +1,8 @@
 use crate::cli::Cli;
 use crate::db::evedb::EveDb;
 use rusqlite::{Connection, Transaction, params, Params};
+use anyhow::Result;
+use calamine::CellErrorType::Value;
 
 pub struct Transacted<'a> {
     sql: String,
@@ -10,8 +12,8 @@ pub struct Transacted<'a> {
 }
 
 impl<'a> Transacted<'a> {
-    pub fn new(cli: &Cli, sql: &str) -> Self {
-        Transacted {
+    pub fn new(cli: & Cli, sql: &str) -> Self {
+        Self {
             sql: sql.to_string(),
             db: EveDb::new(&cli.db_path),
             conn: None,
@@ -19,13 +21,7 @@ impl<'a> Transacted<'a> {
         }
     }
 
-    pub fn execute<P: Params>(&mut self, params: P) -> rusqlite::Result<usize> {
-        if self.conn.is_none() {
-            self.conn = Some(self.db.connect()?);
-        }
-        // if self.tx.is_none() {
-        //     self.tx = Some(self.conn.as_ref().unwrap().transaction()?);
-        // }
-        Ok(0usize)
+    pub fn execute<P: Params>(&mut self, params: P) -> Result<usize> {
+        Ok(0)
     }
 }
